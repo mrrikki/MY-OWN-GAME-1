ef27159c9bbc7a474f7eeeab3df691ad7d36ec52
mrrikki <77407544+mrrikki@users.noreply.github.com>
2021-09-01 07:50:35 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,706 @@
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+
+
+
+  var wall1 = createSprite(340, 398,117,4);
+wall1.shapeColor= "red";
+
+var wall2 = createSprite(283, 340,3,115);
+wall2.shapeColor = "red";
+
+var wall3 = createSprite(215, 281,140,4);
+wall3.shapeColor = "red";
+
+var wall4 = createSprite(145, 268,3,30);
+wall4.shapeColor = "red";
+
+var wall5 = createSprite(225, 395,70,3);
+wall5.shapeColor = "red";
+
+var wall6 = createSprite(190, 360,3,70);
+wall6.shapeColor = "red";
+
+var wall7 = createSprite(165, 323,53,4);
+wall7.shapeColor = "red";
+
+var wall8 = createSprite(140, 337,3,29);
+wall8.shapeColor = "red";
+
+var wall9 = createSprite(155, 350,30,3);
+wall9.shapeColor = "red";
+
+var wall10 = createSprite(247, 370,70,3);
+wall10.shapeColor = "red";
+
+var wall11 = createSprite(170, 373,4,48);
+wall11.shapeColor = "red";
+
+var wall12 = createSprite(212, 336,3,70);
+wall12.shapeColor = "red";
+
+var wall13 = createSprite(190, 300,140,3);
+wall13.shapeColor = "red";
+
+var wall14 = createSprite(260, 325,3,50);
+wall14.shapeColor = "red";
+
+var wall15 = createSprite(245, 350,30,3);
+wall15.shapeColor = "red";
+
+var wall16 = createSprite(233, 336,3,25);
+wall16.shapeColor = "red";
+
+var wall17 = createSprite(120, 335,4,70);
+wall17.shapeColor = "red";
+
+var wall18 = createSprite(132, 370,27,3);
+wall18.shapeColor = "red";
+
+var wall19 = createSprite(145, 382,4,25);
+wall19.shapeColor = "red";
+
+var wall20 = createSprite(70, 395,150,3);
+wall20.shapeColor = "red";
+
+var wall21 = createSprite(3, 383,4,25);
+wall21.shapeColor = "red";
+
+var wall22 = createSprite(39, 373,75,3);
+wall22.shapeColor = "red";
+
+var wall23 = createSprite(75, 340,3,70);
+wall23.shapeColor = "red";
+
+var wall24 = createSprite(87, 303,25,3);
+wall24.shapeColor = "red";
+
+var wall25 = createSprite(97, 337,3,70);
+wall25.shapeColor = "red";
+
+var wall26 = createSprite(350, 370,95,3);
+wall26.shapeColor = "red";
+
+var wall27 = createSprite(303, 300,3,140);
+wall27.shapeColor = "red";
+
+var wall28 = createSprite(350, 233,95,3);
+wall28.shapeColor = "red";
+
+var wall29 = createSprite(360, 255,75,3);
+wall29.shapeColor = "red";
+
+var wall30 = createSprite(323, 303,3,95);
+wall30.shapeColor = "red";
+
+var wall31 = createSprite(360, 350,75,3);
+wall31.shapeColor = "red";
+
+var wall32 = createSprite(395, 315,3,70);
+wall32.shapeColor = "red";
+
+var wall33 = createSprite(370, 280,50,3);
+wall33.shapeColor = "red";
+
+var wall34 = createSprite(348, 305,3,45);
+wall34.shapeColor = "red";
+
+var wall35 = createSprite(360, 325,30,3);
+wall35.shapeColor = "red";
+
+var wall36 = createSprite(373, 315,3,23);
+wall36.shapeColor = "red";
+
+var wall37 = createSprite(30, 350,50,3);
+wall37.shapeColor = "red";
+
+var wall38 = createSprite(360, 350,75,3);
+wall38.shapeColor = "red";
+
+var wall39 = createSprite(30, 327,46,3);
+wall39.shapeColor = "red";
+
+var wall40 = createSprite(55, 320,3,60);
+wall40.shapeColor = "red";
+
+var wall41 = createSprite(5, 315,3,25);
+wall41.shapeColor = "red";
+
+var wall42 = createSprite(88, 290,65,3);
+wall42.shapeColor = "red";
+
+var wall43 = createSprite(18, 303,25,3);
+wall43.shapeColor = "red";
+
+var wall44 = createSprite(30, 270,3,70);
+wall44.shapeColor = "red";
+
+var wall45 = createSprite(50,235,40,3);
+wall45.shapeColor = "red";
+
+var wall46 = createSprite(73, 257,45,3);
+wall46.shapeColor = "red";
+
+var wall47 = createSprite(120, 277,3,25);
+wall47.shapeColor = "red";
+
+var wall48 = createSprite(95, 235,3,45);
+wall48.shapeColor = "red";
+
+var wall49 = createSprite(5, 245,3,70);
+wall49.shapeColor = "red";
+
+var wall50 = createSprite(97, 210,180,3);
+wall50.shapeColor = "red";
+
+var wall51 = createSprite(190, 258,49,3);
+wall51.shapeColor = "red";
+
+var wall52 = createSprite(167, 245,3,30);
+wall52.shapeColor = "red";
+
+var wall53 = createSprite(163, 233,95,3);
+wall53.shapeColor = "red";
+
+var wall54 = createSprite(257, 235,52,3);
+wall54.shapeColor = "red";
+
+var wall55 = createSprite(233, 190,3,90);
+wall55.shapeColor = "red";
+
+var wall56 = createSprite(210, 1,3,10);
+wall56.shapeColor = "red";
+
+var wall57 = createSprite(210, 145,3,135);
+wall57.shapeColor = "red";
+
+var wall58 = createSprite(337, 210,115,3);
+wall58.shapeColor = "red";
+
+var wall59 = createSprite(280, 223,3,25);
+wall59.shapeColor = "red";
+
+var wall60 = createSprite(188, 165,3,90);
+wall60.shapeColor = "red";
+
+var wall61 = createSprite(120, 188,90,3);
+wall61.shapeColor = "red";
+
+var wall62 = createSprite(55, 165,3, 90);
+wall62.shapeColor = "red";
+
+var wall63 = createSprite(12, 190,40,3);
+wall63.shapeColor = "red";
+
+var wall64 = createSprite(30, 178,3,25);
+wall64.shapeColor = "red";
+
+var wall65 = createSprite(12, 165,40,3);
+wall65.shapeColor = "red";
+
+var wall66 = createSprite(395, 135,3,155);
+wall66.shapeColor = "red";
+
+var wall67 = createSprite(258, 190,3,50);
+wall67.shapeColor = "red";
+
+var wall68 = createSprite(280, 190,47,3);
+wall68.shapeColor = "red";
+
+var wall69 = createSprite(350, 190,50,3);
+wall69.shapeColor = "red";
+
+var wall70 = createSprite(268, 165,25,3);
+wall70.shapeColor = "red";
+
+var wall71 = createSprite(165, 177,3,25);
+wall71.shapeColor = "red";
+
+var wall72 = createSprite(143, 165,45,3);
+wall72.shapeColor = "red";
+
+var wall73 = createSprite(76, 155,3,70);
+wall73.shapeColor = "red";
+
+var wall74 = createSprite(304, 145,3,90);
+wall74.shapeColor = "red";
+
+var wall75 = createSprite(98, 143,3,50);
+wall75.shapeColor = "red";
+
+var wall76 = createSprite(85, 120,22,3);
+wall76.shapeColor = "red";
+
+var wall77 = createSprite(120, 130,3,70);
+wall77.shapeColor = "red";
+
+var wall78 = createSprite(30, 120,47,3);
+wall78.shapeColor = "red";
+
+var wall79 = createSprite(348, 145,3,50);
+wall79.shapeColor = "red";
+
+var wall80 = createSprite(373, 142,3,95);
+wall80.shapeColor = "red";
+
+var wall81 = createSprite(360, 120,25,3);
+wall81.shapeColor = "red";
+
+var wall82 = createSprite(360, 95,25,3);
+wall82.shapeColor = "red";
+
+var wall83 = createSprite(20, 142,25,3);
+wall83.shapeColor = "red";
+
+var wall84 = createSprite(5, 75,3,140);
+wall84.shapeColor = "red";
+
+var wall85 = createSprite(325, 135,3,110);
+wall85.shapeColor = "red";
+
+var wall86 = createSprite(283, 99,45,3);
+wall86.shapeColor = "red";
+
+var wall87 = createSprite(280, 142,3,50);
+wall87.shapeColor = "red";
+
+var wall88 = createSprite(245, 145,25,3);
+wall88.shapeColor = "red";
+
+var wall89 = createSprite(260, 75,3,140);
+wall89.shapeColor = "red";
+
+var wall90 = createSprite(153, 142,28,3);
+wall90.shapeColor = "red";
+
+var wall91 = createSprite(142, 130,3,25);
+wall91.shapeColor = "red";
+
+var wall92 = createSprite(165, 120,3,45);
+wall92.shapeColor = "red";
+
+var wall93 = createSprite(120, 95,180,3);
+wall93.shapeColor = "red";
+
+var wall94 = createSprite(30, 65,3,65);
+wall94.shapeColor = "red";
+
+var wall95 = createSprite(328, 75,95,3);
+wall95.shapeColor = "red";
+
+var wall96 = createSprite(383, 57,25,3);
+wall96.shapeColor = "red";
+
+var wall97 = createSprite(235, 60,3,110);
+wall97.shapeColor = "red";
+
+var wall98 = createSprite(140, 78,140,3);
+wall98.shapeColor = "red";
+
+var wall99 = createSprite(55, 50,3,95);
+wall99.shapeColor = "red";
+
+var wall100 = createSprite(315, 55,70,3);
+wall100.shapeColor = "red";
+
+var wall101 = createSprite(350, 40,3,30);
+wall101.shapeColor = "red";
+
+var wall102 = createSprite(375, 30,50,3);
+wall102.shapeColor = "red";
+
+var wall103 = createSprite(75, 40,3,80);
+wall103.shapeColor = "red";
+
+var wall104 = createSprite(15, 5,20,3);
+wall104.shapeColor = "red";
+
+var wall105 = createSprite(165, 65,3,25);
+wall105.shapeColor = "red";
+
+var wall106 = createSprite(130, 53,70,3);
+wall106.shapeColor = "red";
+
+var wall107 = createSprite(210, 55,45,3);
+wall107.shapeColor = "red";
+
+var wall108 = createSprite(305, 6,45,3);
+wall108.shapeColor = "red";
+
+var wall109 = createSprite(325, 20,3,25);
+wall109.shapeColor = "red";
+
+var wall110 = createSprite(315, 30,25,3);
+wall110.shapeColor = "red";
+
+var wall111 = createSprite(369, 10,50,3);
+wall111.shapeColor = "red";
+
+var wall112 = createSprite(97, 17,3,25);
+wall112.shapeColor = "red";
+
+var wall113 = createSprite(85, 5,19,3);
+wall113.shapeColor = "red";
+
+var wall114 = createSprite(165, 8,90,3);
+wall114.shapeColor = "red";
+
+var wall115 = createSprite(280, 30,3,45);
+wall115.shapeColor = "red";
+
+var wall116 = createSprite(165, 30,135,3);
+wall116.shapeColor = "red";
+
+var wall321 = createSprite(340, 398,117,4);
+wall321.shapeColor= "red";
+
+var wall201 = createSprite(283, 340,3,115);
+wall201.shapeColor = "red";
+
+var wall203 = createSprite(215, 281,140,4);
+wall203.shapeColor = "red";
+
+var wall204 = createSprite(145, 268,3,30);
+wall204.shapeColor = "red";
+
+var wall205 = createSprite(625, 395,70,3);
+wall205.shapeColor = "red";
+
+var wall206 = createSprite(590, 360,3,70);
+wall6.shapeColor = "red";
+
+var wall207 = createSprite(565, 323,53,4);
+wall7.shapeColor = "red";
+
+var wall208 = createSprite(540, 337,3,29);
+wall208.shapeColor = "red";
+
+var wall209 = createSprite(555, 350,30,3);
+wall209.shapeColor = "red";
+
+var wall3210 = createSprite(647, 370,70,3);
+wall3210.shapeColor = "red";
+
+var wall3211 = createSprite(570, 373,4,48);
+wall3211.shapeColor = "red";
+
+var wall3212 = createSprite(612, 336,3,70);
+wall3212.shapeColor = "red";
+
+var wall3213 = createSprite(590, 300,140,3);
+wall3213.shapeColor = "red";
+
+var wall3214 = createSprite(660, 325,3,50);
+wall3214.shapeColor = "red";
+
+var wall3215 = createSprite(645, 350,30,3);
+wall3215.shapeColor = "red";
+
+var wall3216 = createSprite(633, 336,3,25);
+wall3216.shapeColor = "red";
+
+var wall3217 = createSprite(520, 335,4,70);
+wall3217.shapeColor = "red";
+
+var wall3218 = createSprite(532, 370,27,3);
+wall3218.shapeColor = "red";
+
+var wall321955 = createSprite(545, 382,4,25);
+wall321955.shapeColor = "red";
+
+var wall2055 = createSprite(470, 395,150,3);
+wall2055.shapeColor = "red";
+
+var wall2155 = createSprite(403, 383,4,25);
+wall2155.shapeColor = "red";
+
+var wall2255 = createSprite(439, 373,75,3);
+wall2255.shapeColor = "red";
+
+var wall2355 = createSprite(475, 340,3,70);
+wall2355.shapeColor = "red";
+
+var wall2455 = createSprite(487, 303,25,3);
+wall2455.shapeColor = "red";
+
+var wall2555 = createSprite(497, 337,3,70);
+wall2555.shapeColor = "red";
+
+var wall2655 = createSprite(750, 370,95,3);
+wall2655.shapeColor = "red";
+
+var wall2755 = createSprite(703, 300,3,140);
+wall2755.shapeColor = "red";
+
+var wall2855 = createSprite(750, 233,95,3);
+wall2855.shapeColor = "red";
+
+var wall2955 = createSprite(760, 255,75,3);
+wall2955.shapeColor = "red";
+
+var wall3055 = createSprite(723, 303,3,95);
+wall3055.shapeColor = "red";
+
+var wall3155 = createSprite(760, 350,75,3);
+wall3155.shapeColor = "red";
+
+var wall3255 = createSprite(795, 315,3,70);
+wall3255.shapeColor = "red";
+
+var wall3355 = createSprite(770, 280,50,3);
+wall3355.shapeColor = "red";
+
+var wall3455 = createSprite(748, 305,3,45);
+wall3455.shapeColor = "red";
+
+var wall3555 = createSprite(760, 325,30,3);
+wall3555.shapeColor = "red";
+
+var wall3655 = createSprite(773, 315,3,23);
+wall3655.shapeColor = "red";
+
+var wall3755 = createSprite(430, 350,50,3);
+wall3755.shapeColor = "red";
+
+var wall3855 = createSprite(760, 350,75,3);
+wall3855.shapeColor = "red";
+
+var wall3955 = createSprite(430, 327,46,3);
+wall3955.shapeColor = "red";
+
+var wall4055 = createSprite(455, 320,3,60);
+wall4055.shapeColor = "red";
+
+var wall4155 = createSprite(405, 315,3,25);
+wall4155.shapeColor = "red";
+
+var wall4255 = createSprite(488, 290,65,3);
+wall4255.shapeColor = "red";
+
+var wall4355 = createSprite(418, 303,25,3);
+wall4355.shapeColor = "red";
+
+var wall4455 = createSprite(430, 270,3,70);
+wall4455.shapeColor = "red";
+
+var wall4555 = createSprite(450,235,40,3);
+wall4555.shapeColor = "red";
+
+var wall4655 = createSprite(473, 257,45,3);
+wall4655.shapeColor = "red";
+
+var wall4755 = createSprite(420, 277,3,25);
+wall4755.shapeColor = "red";
+
+var wall4855 = createSprite(495, 235,3,45);
+wall4855.shapeColor = "red";
+
+var wall4977 = createSprite(405, 245,3,70);
+wall4977.shapeColor = "red";
+
+var wall5077 = createSprite(497, 210,180,3);
+wall5077.shapeColor = "red";
+
+var wall5177 = createSprite(590, 258,49,3);
+wall5177.shapeColor = "red";
+
+var wall5277 = createSprite(567, 245,3,30);
+wall5277.shapeColor = "red";
+
+var wall5377 = createSprite(563, 233,95,3);
+wall5377.shapeColor = "red";
+
+var wall5477 = createSprite(657, 235,52,3);
+wall5477.shapeColor = "red";
+
+var wall5577 = createSprite(633, 190,3,90);
+wall5577.shapeColor = "red";
+
+var wall5677 = createSprite(610, 1,3,10);
+wall5677.shapeColor = "red";
+
+var wall5777 = createSprite(610, 145,3,135);
+wall5777.shapeColor = "red";
+
+var wall5877 = createSprite(737, 210,115,3);
+wall5877.shapeColor = "red";
+
+var wall5977 = createSprite(680, 223,3,25);
+wall5977.shapeColor = "red";
+
+var wall6077 = createSprite(588, 165,3,90);
+wall6077.shapeColor = "red";
+
+var wall6177 = createSprite(520, 188,90,3);
+wall6177.shapeColor = "red";
+
+var wall6277 = createSprite(455, 165,3, 90);
+wall6277.shapeColor = "red";
+
+var wall6377 = createSprite(412, 190,40,3);
+wall63.shapeColor = "red";
+
+var wall6477 = createSprite(430, 178,3,25);
+wall64.shapeColor = "red";
+
+var wall6577 = createSprite(412, 165,40,3);
+wall6577.shapeColor = "red";
+
+var wall6677 = createSprite(795, 135,3,155);
+wall6677.shapeColor = "red";
+
+var wall6777 = createSprite(658, 190,3,50);
+wall6777.shapeColor = "red";
+
+var wall6877 = createSprite(680, 190,47,3);
+wall6877.shapeColor = "red";
+
+var wall6977 = createSprite(750, 190,50,3);
+wall6977.shapeColor = "red";
+
+var wall7077 = createSprite(668, 165,25,3);
+wall7077.shapeColor = "red";
+
+var wall7177 = createSprite(565, 177,3,25);
+wall7177.shapeColor = "red";
+
+var wall7277 = createSprite(543, 165,45,3);
+wall7277.shapeColor = "red";
+
+var wall7377 = createSprite(476, 155,3,70);
+wall7377.shapeColor = "red";
+
+var wall7477 = createSprite(704, 145,3,90);
+wall7477.shapeColor = "red";
+
+var wall7577 = createSprite(498, 143,3,50);
+wall7577.shapeColor = "red";
+
+var wall7677 = createSprite(485, 120,22,3);
+wall7677.shapeColor = "red";
+
+var wall7777 = createSprite(420, 130,3,70);
+wall7777.shapeColor = "red";
+
+var wall7877 = createSprite(430, 120,47,3);
+wall7877.shapeColor = "red";
+
+var wall7977 = createSprite(748, 145,3,50);
+wall7977.shapeColor = "red";
+
+var wall8077 = createSprite(773, 142,3,95);
+wall8077.shapeColor = "red";
+
+var wall8177 = createSprite(760, 120,25,3);
+wall8177.shapeColor = "red";
+
+var wall8277 = createSprite(760, 95,25,3);
+wall8277.shapeColor = "red";
+
+var wall8377 = createSprite(420, 142,25,3);
+wall8377.shapeColor = "red";
+
+var wall8477 = createSprite(405, 75,3,140);
+wall8477.shapeColor = "red";
+
+var wall8577 = createSprite(725, 135,3,110);
+wall8577.shapeColor = "red";
+
+var wall8677 = createSprite(683, 99,45,3);
+wall8677.shapeColor = "red";
+
+var wall8777 = createSprite(680, 142,3,50);
+wall8777.shapeColor = "red";
+
+var wall8877 = createSprite(645, 145,25,3);
+wall8877.shapeColor = "red";
+
+var wall8977 = createSprite(660, 75,3,140);
+wall8977.shapeColor = "red";
+
+var wall9077 = createSprite(553, 142,28,3);
+wall9077.shapeColor = "red";
+
+var wall9177 = createSprite(542, 130,3,25);
+wall9177.shapeColor = "red";
+
+var wall9266 = createSprite(565, 120,3,45);
+wall9266.shapeColor = "red";
+
+var wall9355 = createSprite(520, 95,180,3);
+wall9355.shapeColor = "red";
+
+var wall9444 = createSprite(430, 65,3,65);
+wall9444.shapeColor = "red";
+
+var wall9533 = createSprite(728, 75,95,3);
+wall9533.shapeColor = "red";
+
+var wall9622 = createSprite(783, 57,25,3);
+wall9622.shapeColor = "red";
+
+var wall9724 = createSprite(635, 60,3,110);
+wall9724.shapeColor = "red";
+
+var wall98767 = createSprite(540, 78,140,3);
+wall98767.shapeColor = "red";
+
+var wall9999 = createSprite(455, 50,3,95);
+wall9999.shapeColor = "red";
+
+var wall32100 = createSprite(715, 55,70,3);
+wall32100.shapeColor = "red";
+
+var wall32101 = createSprite(750, 40,3,30);
+wall32101.shapeColor = "red";
+
+var wall32102 = createSprite(775, 30,50,3);
+wall32102.shapeColor = "red";
+
+var wall32103 = createSprite(475, 40,3,80);
+wall32103.shapeColor = "red";
+
+var wall32104 = createSprite(415, 5,20,3);
+wall32104.shapeColor = "red";
+
+var wall32105 = createSprite(565, 65,3,25);
+wall32105.shapeColor = "red";
+
+var wall32106 = createSprite(530, 53,70,3);
+wall32106.shapeColor = "red";
+
+var wall32107 = createSprite(610, 55,45,3);
+wall32107.shapeColor = "red";
+
+var wall32108 = createSprite(705, 6,45,3);
+wall32108.shapeColor = "red";
+
+var wall32109 = createSprite(725, 20,3,25);
+wall32109.shapeColor = "red";
+
+var wall32110 = createSprite(715, 30,25,3);
+wall32110.shapeColor = "red";
+
+var wall32111 = createSprite(769, 10,50,3);
+wall32111.shapeColor = "red";
+
+var wall32112 = createSprite(497, 17,3,25);
+wall32112.shapeColor = "red";
+
+var wall32113 = createSprite(485, 5,19,3);
+wall32113.shapeColor = "red";
+
+var wall32114 = createSprite(565, 8,90,3);
+wall32114.shapeColor = "red";
+
+var wall32115 = createSprite(680, 30,3,45);
+wall32115.shapeColor = "red";
+
+var wall32116 = createSprite(565, 30,135,3);
+wall32116.shapeColor = "red";
+}
+
+function draw() {
+  background(0);  
+  drawSprites();
+}
